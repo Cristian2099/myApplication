4f0ad6e66c53adfe8003210546a6763673010735
cristian2099 <cristian.d5299@gmail.com>
2018-05-22 07:25:49 -0500

    Pruebas

---
+++ app/src/main/java/programacion/proyecto/cristian/myapplication/MainActivity.java
@@ -0,0 +1,26 @@
+package programacion.proyecto.cristian.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button boton = (Button)findViewById(R.id.boton);
+        boton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intento = new Intent(v.getContext(),SecondActivity.class);
+                startActivityForResult(intento,0);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/programacion/proyecto/cristian/myapplication/SecondActivity.java
@@ -0,0 +1,15 @@
+package programacion.proyecto.cristian.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+
+public class SecondActivity extends AppCompatActivity{
+
+    @Override
+    public void onCreate(Bundle saveInstanceState){
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout.second_activity);
+    }
+}
